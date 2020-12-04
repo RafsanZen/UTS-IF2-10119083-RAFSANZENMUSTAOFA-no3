86f1fdf1a9061bd2c356131d27825116124ef71e
Rafsan Zen M <73044188+RafsanZen@users.noreply.github.com>
2020-12-04 21:57:51 +0700

    Add files via upload

---
+++ UTS-IF2-10119083-RAFSANZENMUSTAOFA-no3/src/uts/if2/pkg10119083/rafsanzenmustaofa/no3/UTSIF210119083RAFSANZENMUSTAOFANo3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.if2.pkg10119083.rafsanzenmustaofa.no3;
+
+/**
+ *
+ * @author hendrawan
+ */
+public class UTSIF210119083RAFSANZENMUSTAOFANo3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
